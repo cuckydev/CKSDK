cc691a57a609963bf26d6cf46b631ea36968d4a8
cuckydev <regan_green27@hotmail.com>
2023-05-07 10:06:29 +0000

    Deploying to doxygen from @ cuckydev/CKSDK@440c367060776a80e0d9ff1de9d7888334aff1eb 🚀

--- search/all_0.js
+++ search/all_0.js
@@ -5,7 +5,7 @@ var searchData=
   ['addr_2',['addr',['../d3/d9c/structCKSDK_1_1OS_1_1SPU__VOICE__CTRL.html#a707550f4f0b64432067f099d5a8910e6',1,'CKSDK::OS::SPU_VOICE_CTRL::addr()'],['../d9/d1e/structCKSDK_1_1SPI_1_1PadPacket.html#ab4a31ae26267a15c3d4f0745f78ddd4c',1,'CKSDK::SPI::PadPacket::addr()']]],
   ['adsr_3',['adsr',['../d3/d9c/structCKSDK_1_1OS_1_1SPU__VOICE__CTRL.html#a49a9fbc2aec70cefef8834eea45562fb',1,'CKSDK::OS::SPU_VOICE_CTRL']]],
   ['alloc_4',['Alloc',['../db/dd5/namespaceCKSDK_1_1Mem.html#add55e8bba2c73b30c1a0ab6f3d50aa42',1,'CKSDK::Mem']]],
-  ['allocpacket_5',['AllocPacket',['../d8/d2a/namespaceCKSDK_1_1GPU.html#aff79ac26014abd75f2964798512e543b',1,'CKSDK::GPU::AllocPacket(size_t ot, size_t words)'],['../d8/d2a/namespaceCKSDK_1_1GPU.html#a05931672a873b090ef0737d08af8030d',1,'CKSDK::GPU::AllocPacket(size_t ot)']]],
+  ['allocpacket_5',['AllocPacket',['../d8/d2a/namespaceCKSDK_1_1GPU.html#a8306dfbb814b19ae2a0811373d76edcf',1,'CKSDK::GPU::AllocPacket(size_t ot, size_t words)'],['../d8/d2a/namespaceCKSDK_1_1GPU.html#a6a37a54ced2e46714c59443268976d41',1,'CKSDK::GPU::AllocPacket(size_t ot)']]],
   ['america_6',['America',['../dc/d2b/namespaceCKSDK_1_1CD.html#aabe89ede4a564e2e9533eff996fbb41fa088f003833d523d9dccc529e929afdc7',1,'CKSDK::CD']]],
   ['analog_7',['analog',['../dc/dd8/structCKSDK_1_1SPI_1_1PadResponse.html#afdc89f379c77fb627466f9e8cd7a9e8d',1,'CKSDK::SPI::PadResponse']]],
   ['analog_8',['Analog',['../d5/d0b/namespaceCKSDK_1_1SPI.html#a8fadab7f8bbc7a54e79b3e6e858a0da9aa3002b111f6b8eac54284351e975c857',1,'CKSDK::SPI']]],
--- search/functions_0.js
+++ search/functions_0.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['abort_636',['Abort',['../d3/d1d/namespaceCKSDK_1_1ExScreen.html#a1c803edf8d79a21628d2e8a41cc66c89',1,'CKSDK::ExScreen']]],
   ['alloc_637',['Alloc',['../db/dd5/namespaceCKSDK_1_1Mem.html#add55e8bba2c73b30c1a0ab6f3d50aa42',1,'CKSDK::Mem']]],
-  ['allocpacket_638',['AllocPacket',['../d8/d2a/namespaceCKSDK_1_1GPU.html#aff79ac26014abd75f2964798512e543b',1,'CKSDK::GPU::AllocPacket(size_t ot, size_t words)'],['../d8/d2a/namespaceCKSDK_1_1GPU.html#a05931672a873b090ef0737d08af8030d',1,'CKSDK::GPU::AllocPacket(size_t ot)']]]
+  ['allocpacket_638',['AllocPacket',['../d8/d2a/namespaceCKSDK_1_1GPU.html#a8306dfbb814b19ae2a0811373d76edcf',1,'CKSDK::GPU::AllocPacket(size_t ot, size_t words)'],['../d8/d2a/namespaceCKSDK_1_1GPU.html#a6a37a54ced2e46714c59443268976d41',1,'CKSDK::GPU::AllocPacket(size_t ot)']]]
 ];
