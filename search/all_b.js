3032bb00e8235ac7e1970954a588a81839d7775d
cuckydev <regan_green27@hotmail.com>
2023-05-07 07:40:44 +0000

    Deploying to doxygen from @ cuckydev/CKSDK@61cb1e570283badfd916d3c13e5b6d673b3946d5 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -9,7 +9,7 @@ var searchData=
   ['mdecout_319',['MDECOUT',['../db/d79/namespaceCKSDK_1_1OS.html#aeca51feef485f5889a4f6d93690a9eabac75c554addfd7f170218cd9bdeff1d5f',1,'CKSDK::OS']]],
   ['mem_2eh_320',['Mem.h',['../de/d56/Mem_8h.html',1,'']]],
   ['minute_321',['minute',['../df/d17/structCKSDK_1_1CD_1_1Loc.html#a781b2460e1b532fe8d87dd991bb92f24',1,'CKSDK::CD::Loc::minute()'],['../d2/d5e/unionCKSDK_1_1CD_1_1DAReport.html#a80792d369f9b76ccadc109c3eff0379a',1,'CKSDK::CD::DAReport::minute()']]],
-  ['mode_322',['mode',['../dc/d0d/structCKSDK_1_1GPU_1_1Buffer_1_1GP0.html#a2d4f81a4ff5553336bfa77fab5a790ee',1,'CKSDK::GPU::Buffer::GP0::mode()'],['../da/de2/structCKSDK_1_1OS_1_1SIO__CTRL.html#af68979365d2b7819e8846b41a7517e2d',1,'CKSDK::OS::SIO_CTRL::mode()']]],
+  ['mode_322',['mode',['../dc/d0d/structCKSDK_1_1GPU_1_1Buffer_1_1GP0.html#a391b41574285550c3c364365d9c69a91',1,'CKSDK::GPU::Buffer::GP0::mode()'],['../da/de2/structCKSDK_1_1OS_1_1SIO__CTRL.html#af68979365d2b7819e8846b41a7517e2d',1,'CKSDK::OS::SIO_CTRL::mode()']]],
   ['mode_323',['Mode',['../dc/d2b/namespaceCKSDK_1_1CD.html#a0b496e9e5f753107893cc47c07e91954',1,'CKSDK::CD']]],
   ['motor_5fl_324',['motor_l',['../d9/d1e/structCKSDK_1_1SPI_1_1PadPacket.html#a46516f696da9be58cc4a429f2787348d',1,'CKSDK::SPI::PadPacket']]],
   ['motor_5fr_325',['motor_r',['../d9/d1e/structCKSDK_1_1SPI_1_1PadPacket.html#a366c7127d3283cf23706ea50ca352fc3',1,'CKSDK::SPI::PadPacket']]],
